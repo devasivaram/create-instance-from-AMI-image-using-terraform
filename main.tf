d1a849cd28dae8e1c5a88ce0c350d694859909dc
Devanand TS <100773863+devasivaram@users.noreply.github.com>
2022-04-08 20:25:32 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "aws_instance"  "myapp-instance" {
+
+  ami                     =    data.aws_ami.myapp-ami.id
+  instance_type           =    "t2.micro"
+  key_name                =    aws_key_pair.devopsnew.id
+  vpc_security_group_ids  =    ["add your sec.grp ID"]
+  tags = {
+    Name = "myapp"
+  }
+}
+
+resource "aws_key_pair" "devopsnew" {
+
+  key_name = "devopsnew1"
+  public_key = file("path to key.pub")
+  tags = {
+    Name = "devopsnew1"
+  }
+}
